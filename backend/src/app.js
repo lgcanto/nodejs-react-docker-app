679a1c14cadb8223f01941abcf6c0b7ca651522d
lgcanto <lucascanto@poli.ufrj.br>
2020-06-28 16:53:51 -0300

    Testing heroku CD once again

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ var app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World! (Testing Heroku CD)');
+  res.send('Hello World! (Testing Heroku CD 2)');
 });
 
 app.listen(PORT, function () {
